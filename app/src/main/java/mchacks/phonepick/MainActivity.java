05355186e5d07d11b92cd35b0a043df7c65eec4d
Li Xuanji <xuanji@gmail.com>
2015-02-21 14:08:33 -0500

    Get list of peers on startup

--- app/src/main/java/mchacks/phonepick/MainActivity.java
+++ app/src/main/java/mchacks/phonepick/MainActivity.java
@@ -2,9 +2,11 @@ package mchacks.phonepick;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +17,7 @@ public class MainActivity extends ActionBarActivity
     WifiP2pManager wifiManager;
     WifiP2pManager.Channel wifiChannel;
     BroadcastReceiver wifiBroadCastReceiver;
+    IntentFilter mIntentFilter;
 
 
     @Override
@@ -27,21 +30,34 @@ public class MainActivity extends ActionBarActivity
         wifiChannel = wifiManager.initialize(this, getMainLooper(), null);
         wifiBroadCastReceiver = new WiFiDirectBroadcastReceiver(wifiManager, wifiChannel, this);
 
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        wifiManager.discoverPeers(wifiChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.w("wifi", "success");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Log.w("wifi", "failure");
+            }
+        });
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -50,5 +66,17 @@ public class MainActivity extends ActionBarActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(wifiBroadCastReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(wifiBroadCastReceiver);
     }
 }
